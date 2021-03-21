3f1eb601afd94b139cdad4cc12308aac9f288ac8
Pavel Potapov <paveln0366@gmail.com>
2021-03-21 12:59:22 +0300

    Studied: access modifiers

---
+++ src/oop/lesson_12_access_modifiers/Main.kt
@@ -0,0 +1,8 @@
+package oop.lesson_12_access_modifiers
+
+fun main() {
+    println("private - доступен внутри класса или файла")
+    println("public - доступен везде, установлен по умолчанию")
+    println("protected - доступен внутри класса или файла, а также у наследников")
+    println("internal - доступен внутри модуля")
+}
